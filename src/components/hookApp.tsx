6a6ce74418b4ee51d083ae2eb7408158949f520e
uuronn <uuronprogram@gmail.com>
2022-03-10 02:56:46 +0900

    コンポーネントを動かした

--- src/components/hookApp.tsx
+++ src/components/hookApp.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import { useEffect, useState } from 'react'
 // import './App.css'
 import { useInteractJS } from './hooks'
 import { css, SerializedStyles } from '@emotion/react'
@@ -12,79 +12,21 @@ const hoge = css`
 
 const App: React.FC = () => {
   const interact = useInteractJS()
-  // console.log(interact)
-  // console.log(interact.position.width)
-  // interact.position.width = 300
-  // console.log(interact.position.width)
-  // interact.position.x = 300
-//   const cardRote = css`
-//     box-sizing: border-box;
-//     position: relative;
-//     z-index: 0;
-//     width: 400px;
-//     height: 300px;
-//     margin:5px auto;
-//     border-radius: 10px;
-//     overflow: hidden;
-//     padding: 2rem;
+  const [time, setTime] = useState(0);
+  const random = Math.floor(Math.random());
+  console.log(random)
 
-//     @-webkit-keyframes rotate {
-//     100% {
-//       -webkit-transform: rotate(1turn);
-//       transform: rotate(1turn);
-//     }
-// }
-//   @keyframes rotate {
-//     100% {
-//       -webkit-transform: rotate(1turn);
-//       transform: rotate(1turn);
-//     }
-// }
+    useEffect(() => {
+      const id = setInterval(() => {
+        setTime(time + 1);
+        interact.position.x++
+        // interact.position.y++
+        // console.log(`x座標${interact.position.x}`)
+      }, 80);
+      return () => clearInterval(id);
+    }, [time]);
 
-//     .rainbow::before {
-//     content: '';
-//     position: absolute;
-//     z-index: -2;
-//     left: -50%;
-//     top: -50%;
-//     width: 200%;
-//     height: 200%;
-//     background-color: #399953;
-//     background-repeat: no-repeat;
-//     background-size: 50% 50%, 50% 50%;
-//     background-position: 0 0, 100% 0, 100% 100%, 0 100%;
-//     background-image: linear-gradient(#399953, #399953), linear-gradient(#fbb300, #fbb300), linear-gradient(#d53e33, #d53e33), linear-gradient(#377af5, #377af5);
-//     -webkit-animation: rotate 4s linear infinite;
-//     animation: rotate 4s linear infinite;
-// }
-// .rainbow::after {
-//     content: '';
-//     position: absolute;
-//     z-index: -1;
-//     left: 6px;
-//     top: 6px;
-//     width: calc(100% - 12px);
-//     height: calc(100% - 12px);
-//     background: white;
-//     border-radius: 5px;
-// // }
-//   `
-  
-//   const card = css`
-//     background: red;
-//     border: solid 1px red;
-//     &:hover {
-//       border: solid 1px blue;
-//     }
-//   `
-  
-//   const input = css`
-//     color: blue;
-//     border: medium #c0c0c0 dotted;
-//     &:hover {
-//       color: red;
-//       border: solid 1px red;
-//     }
+    // console.log(time)
 
   return (
     <div className="App">
@@ -92,7 +34,7 @@ const App: React.FC = () => {
       <button onClick={() => interact.disable()}>無効化</button>
       <input css={hoge}
         type="text"
-        // value="fdfdf"
+        value="動いていくー"
         ref={interact.ref}
         style={{
           ...interact.style,
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,18 +1,23 @@
 import type { NextPage } from 'next'
 import Card from '../components/shared/Card'
 import App from '../components/hookApp'
-import Timer from '../components/timer'
+// import Timer from '../components/timer'
+
+const window = {
+  width: "100vw",
+  height: "100vh"
+}
 
 const Home: NextPage = () => {
   return (
-    <>
+    <div style={window}>
       <h1>Hello Next.js</h1>
       <Card />
       
       <App/>
-      <App/>
-      <Timer/>
-    </>
+      {/* <App/> */}
+      {/* <Timer/> */}
+    </div>
   )
 }
 
